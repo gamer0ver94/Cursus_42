ebddb078c11e9629a2da2339663fd9f434b81f19
gamer0ver94 <duarte.paulino94@hotmail.com>
2023-01-22 19:55:23 +0100

    debugged some stuff

--- c++_modules/module00/ex02/Account.cpp
+++ c++_modules/module00/ex02/Account.cpp
@@ -1,6 +1,8 @@
 #include "Account.hpp"
 #include <iostream>
 #include<ctime>
+#include <chrono>
+#include <iomanip>
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
@@ -19,13 +21,13 @@ Account::Account(int initial_deposit){
 	this->_accountIndex = this->_nbAccounts;
 	this->_nbAccounts++;
 	this->_totalAmount += initial_deposit;
-	std::cout << "index:" << this->_accountIndex << ":amount:" << this->_amount << ";created" << std::endl;
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
 }
 
 Account::~Account(void){
 	this->_displayTimestamp();
 	this->_nbAccounts--;
-		std::cout << "index:" << this->_accountIndex << ":amount:" << this->_amount << ";closed" << std::endl;
+		std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
 }
 
 void Account::makeDeposit( int deposit ){
@@ -35,10 +37,10 @@ void Account::makeDeposit( int deposit ){
 	this->_totalNbDeposits++;
 }
 
-bool Account::makeWithdrawal( int withdrawal ){
+bool Account::makeWithdrawal(int withdrawal ){
 	if (this->_amount - withdrawal < 0)
 		return (false);
-	this->_totalAmount -= withdrawal;
+	this->_amount -= withdrawal;
 	this->_nbWithdrawals++;
 	this->_totalNbWithdrawals++;
 	return (true);
@@ -50,12 +52,12 @@ int Account::checkAmount( void ) const{
 
 void Account::displayStatus( void ) const{
 	this->_displayTimestamp();
-	std::cout << "index:" << this->_accountIndex << ":amount:" << this->_amount << ";deposits:" << this->getNbDeposits() << ";withdrawals:" << this->getNbWithdrawals() << std::endl;
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
 }
 
 void	Account::displayAccountsInfos( void ){
 	_displayTimestamp();
-	std::cout << "accounts:" << getNbAccounts() << ":total:" << getTotalAmount() << ":deposits:" << getNbDeposits() << ":withdrawals:" << getNbWithdrawals() << std::endl;
+	std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount() << ":deposits:" << getNbDeposits() << ":withdrawals:" << getNbWithdrawals() << std::endl;
 }
 
 int	Account::getNbAccounts( void ){
@@ -76,9 +78,11 @@ int	Account::getNbWithdrawals( void ){
 
 void	Account::_displayTimestamp( void ){
 	std::time_t currentTime;
+	tm *timeStruct;
+	char date[9];
 
-	struct tm *tTime;
-	time(&currentTime);
-	tTime = localtime(&currentTime);
-	std::cout << "[" << asctime(tTime) << "] ";
+	currentTime = time(0);
+	timeStruct = localtime(&currentTime);
+	strftime(date, 9,"%Y%m%d", timeStruct);
+	std::cout << date << " ";
 }
--- c++_modules/module01/ex04/main.cpp
+++ c++_modules/module01/ex04/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <fstream>
+#define WHITE "\033[0;37m"
+#define BLUE "\033[0;34m"
+#define GREEN "\033[0;32m"
+#define CYAN "\033[0;36m"
+#define RED "\033[0;31m"
+#define YELLOW "\033[0;33m"
 
 int main(int argc ,char **argv){
 
@@ -7,24 +13,26 @@ int main(int argc ,char **argv){
 	std::ofstream outFile;
     std::string line;
 	std::size_t found;
-	std::string add = argv[3];
-	std::string del = argv[2];
 	std::string fileToBeCreated;
-
-	fileToBeCreated.append(argv[1]).append(".replace");
+	int i = 0;
 	found = 0;
     if (argc < 4){
-        std::cout << "No args" << std::endl;
+        std::cout << RED << "Not enought args to run the program ..." << WHITE << std::endl;
         return (-1);
     }
+	std::string add = argv[3];
+	std::string del = argv[2];
+	fileToBeCreated.append(argv[1]).append(".replace");
     inFile.open(argv[1]);
 	outFile.open(fileToBeCreated);
     if (!inFile.good()){
-        std::cout << "Error no file" << std::endl;
+        std::cout << RED << "File does not exist ..." << std::endl;
+		inFile.close();
+		outFile.close();
         return (-1);
     }
     if (inFile.is_open())
-     std::cout << "its open" << std::endl;
+     std::cout << GREEN << "File open with sucess..." << WHITE << std::endl;
 	while (!inFile.eof()) {
    		getline(inFile, line);
 		found = line.find(argv[2]); 
@@ -32,10 +40,15 @@ int main(int argc ,char **argv){
 		{
 			line.erase(found, del.length());
 			line.insert(found, add);
+			i++;
 		}
 		outFile << line << std::endl;
 	}
+	if (i > 0){
+		std::cout << CYAN << i << " word(s)replaced by " << argv[3] << WHITE << std::endl;
+	}
     inFile.close();
 	outFile.close();
+	std::cout << GREEN << "File closed with success" << WHITE << std::endl;
     return (0);
 }
