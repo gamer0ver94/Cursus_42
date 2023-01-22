53e42acb84127bcc8ed827b592153270e455a119
gamer0ver94 <duarte.paulino94@hotmail.com>
2023-01-22 18:48:43 +0100

    module 00  ex02 and module  01 ex04

---
+++ c++_modules/module00/ex02/Account.cpp
@@ -0,0 +1,84 @@
+#include "Account.hpp"
+#include <iostream>
+#include<ctime>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(void){
+	this->_displayTimestamp();
+	this->_accountIndex = this->_nbAccounts;
+	this->_nbAccounts++;
+}
+
+Account::Account(int initial_deposit){
+	this->_displayTimestamp();
+	this->_amount = initial_deposit;
+	this->_accountIndex = this->_nbAccounts;
+	this->_nbAccounts++;
+	this->_totalAmount += initial_deposit;
+	std::cout << "index:" << this->_accountIndex << ":amount:" << this->_amount << ";created" << std::endl;
+}
+
+Account::~Account(void){
+	this->_displayTimestamp();
+	this->_nbAccounts--;
+		std::cout << "index:" << this->_accountIndex << ":amount:" << this->_amount << ";closed" << std::endl;
+}
+
+void Account::makeDeposit( int deposit ){
+	this->_amount += deposit;
+	this->_totalAmount += deposit;
+	this->_nbDeposits++;
+	this->_totalNbDeposits++;
+}
+
+bool Account::makeWithdrawal( int withdrawal ){
+	if (this->_amount - withdrawal < 0)
+		return (false);
+	this->_totalAmount -= withdrawal;
+	this->_nbWithdrawals++;
+	this->_totalNbWithdrawals++;
+	return (true);
+}
+
+int Account::checkAmount( void ) const{
+	return (this->_amount);
+}
+
+void Account::displayStatus( void ) const{
+	this->_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ":amount:" << this->_amount << ";deposits:" << this->getNbDeposits() << ";withdrawals:" << this->getNbWithdrawals() << std::endl;
+}
+
+void	Account::displayAccountsInfos( void ){
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ":total:" << getTotalAmount() << ":deposits:" << getNbDeposits() << ":withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+int	Account::getNbAccounts( void ){
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void ){
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void ){
+	return(_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void ){
+	return (_totalNbWithdrawals);
+}
+
+void	Account::_displayTimestamp( void ){
+	std::time_t currentTime;
+
+	struct tm *tTime;
+	time(&currentTime);
+	tTime = localtime(&currentTime);
+	std::cout << "[" << asctime(tTime) << "] ";
+}
--- c++_modules/module01/ex04/main.cpp
+++ c++_modules/module01/ex04/main.cpp
@@ -4,25 +4,38 @@
 int main(int argc ,char **argv){
 
     std::ifstream inFile;
+	std::ofstream outFile;
     std::string line;
+	std::size_t found;
+	std::string add = argv[3];
+	std::string del = argv[2];
+	std::string fileToBeCreated;
 
+	fileToBeCreated.append(argv[1]).append(".replace");
+	found = 0;
     if (argc < 4){
         std::cout << "No args" << std::endl;
         return (-1);
     }
     inFile.open(argv[1]);
+	outFile.open(fileToBeCreated);
     if (!inFile.good()){
         std::cout << "Error no file" << std::endl;
         return (-1);
     }
     if (inFile.is_open())
      std::cout << "its open" << std::endl;
-    getline(inFile, line);
-while (!inFile.eof()) {
-    std::cout << line << std::endl;
-    getline(inFile, line);
-}
+	while (!inFile.eof()) {
+   		getline(inFile, line);
+		found = line.find(argv[2]); 
+		if (found != std::string::npos)
+		{
+			line.erase(found, del.length());
+			line.insert(found, add);
+		}
+		outFile << line << std::endl;
+	}
     inFile.close();
-    std::cout << line << std::endl;
+	outFile.close();
     return (0);
 }
