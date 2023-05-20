93331bb1a48abee34681d9fc33de4b2f73035413
gamer0ver94 <duarte.paulino94@hotmail.com>
2023-05-20 17:38:56 +0200

    another

--- c++_modules/module08/ex00/main.cpp
+++ c++_modules/module08/ex00/main.cpp
@@ -3,37 +3,39 @@
 #include <list>
 #include <algorithm>
 #include <stdexcept>
-
+#include <string>
 
 #include "generic/easyfind.hpp"
 int main() {
-    std::vector<int> vec;
-    std::list<int> lst;
-	vec.push_back(1);
-	vec.push_back(2);
-	vec.push_back(3);
-	vec.push_back(4);
-	vec.push_back(5);
-	lst.push_back(10);
-	lst.push_back(20);
-	lst.push_back(30);
-	lst.push_back(40);
-	lst.push_back(50);
-
-    try {
-        std::vector<int>::iterator it_vec = easyfind(vec, 3);
-        std::cout << "Value found in vector at index " << it_vec - vec.begin() << std::endl;
-    }
-    catch (std::exception& e) {
-        std::cout << e.what() << std::endl;
-    }
-    try {
-        std::list<int>::iterator it_lst = easyfind(lst, 50);
-        std::cout << "Value found in list at position " << std::distance(lst.begin(), it_lst) << std::endl;
-    }
-    catch (std::exception& e) {
-        std::cout << e.what() << std::endl;
-    }
-
+	std::string line;
+    std::vector<int> test;
+	std::string elem;
+	system("clear");
+	while (1){
+		std::cout << " PLEASE ENTER ONE ELEMENT AT A TIME // TYPE <DONE> WHEN IT FINISHED" << std::endl;
+		std::getline(std::cin,  line);
+		if (line == "DONE"){
+			break;
+		}
+		try{
+			test.push_back(std::stoi(line));
+		}
+		catch(...){
+		}
+		system("clear");
+	}
+	while (1)
+	{
+		std::cout << "TYPE A ELEMENT VALUE TO SEARCH" << std::endl;
+		std::getline(std::cin, elem);
+		 try {
+       		 std::vector<int>::iterator it = easyfind(test, std::stoi(elem));
+       		 std::cout << "Value found in vector at index " << it - test.begin() << std::endl;
+    	}
+   		 catch (std::exception& e) {
+       		 std::cout << e.what() << std::endl;
+   	 }
+	}
+   
     return 0;
 }
--- c++_modules/module08/ex01/main.cpp
+++ c++_modules/module08/ex01/main.cpp
@@ -1,7 +1,7 @@
 #include "classes/Span.hpp"
 #include <cstdlib>
 #include <ctime>
-#define MAX_SPAN 10000
+#define MAX_SPAN 100000
 
 int main(void){
 Span sp = Span(MAX_SPAN);
@@ -12,7 +12,20 @@ for(int i = 0; i < MAX_SPAN; i++){
     random = rand() % 100000000;
     sp.addNumber(random);
 }
-std::cout << sp.shortestSpan() << std::endl;
-std::cout << sp.longestSpan() << std::endl;
+std::cout << "shortest span " << sp.shortestSpan() << std::endl;
+std::cout << "longes spam " << sp.longestSpan() << std::endl;
     return 0;
 }
+
+// int main()
+// {
+// Span sp = Span(5);
+// sp.addNumber(6);
+// sp.addNumber(3);
+// sp.addNumber(17);
+// sp.addNumber(9);
+// sp.addNumber(11);
+// std::cout << sp.shortestSpan() << std::endl;
+// std::cout << sp.longestSpan() << std::endl;
+// return 0;
+// }
