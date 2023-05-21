ca3c832ccf81205da86d65912e6701de70cc88f7
gamer0ver94 <duarte.paulino94@hotmail.com>
2023-05-21 15:41:59 +0200

    debugged ex01

--- c++_modules/module08/ex01/main.cpp
+++ c++_modules/module08/ex01/main.cpp
@@ -4,15 +4,36 @@
 #define MAX_SPAN 10000
 
 int main(void){
-Span sp = Span(MAX_SPAN);
-std::srand(std::time(NULL));
-int random;
+    Span sp = Span(MAX_SPAN);
+    std::srand(std::time(NULL));
+    int random;
+    try{
+        for(int i = 0; i < MAX_SPAN; i++){
+        random = std::rand() % 100000000;
+        sp.addNumber(random);
+        }
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
+    }
+    catch(std::exception &e){
+        std::cout << "error: " << e.what() << std::endl;
+    };
+    
+    
+    std::cout << "_______________________" << std::endl;
+    Span spTmp = Span(10000000);
+    std::vector<int> test;
+    try{
+        for(int i = 0; i < 10000000; i++){
+        test.push_back(i);
+        }
+        spTmp.addNumber(test);
+        std::cout << spTmp.shortestSpan() << std::endl;
+        std::cout << spTmp.longestSpan() << std::endl;
+    }
+    catch(std::exception &e){
+        std::cout << "error: " << e.what() << std::endl;
+    }
 
-for(int i = 0; i < MAX_SPAN; i++){
-    random = rand() % 100000000;
-    sp.addNumber(random);
-}
-std::cout << sp.shortestSpan() << std::endl;
-std::cout << sp.longestSpan() << std::endl;
     return 0;
 }
